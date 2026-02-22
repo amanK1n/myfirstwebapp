666790b34539774b237836e3fe9c037d3b6864a9
amanK1n <amankonen0799@gmail.com>
2026-02-22 16:13:04 +0530

    Added starter validation

--- src/main/java/com/aman/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/aman/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.aman.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -22,10 +25,26 @@ public class TodoController {
 		return "listTodos";
 	}
 	
-	@RequestMapping("add-todo")
-	public String showNewTodoPage() {
-		
-		return "listTodos";
+	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.POST)
+	public String addNewTodo(ModelMap model, Todo todo) {
+		String username = (String)model.get("name2");
+		System.out.println("#######********_____==++++==_____#######********");
+		System.out.println("#######********_____==++++==_____#######********");
+		System.out.println("Username from TODO page " + username);
+		System.out.println("Username from TODO page " + username);
+		System.out.println("Username from TODO page " + username);
+		System.out.println("#######********_____==++++==_____#######********");
+		System.out.println("#######********_____==++++==_____#######********");
+		todoService.addTodo(username , todo.getDescription(), LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
 	}
 	
 }
--- src/main/java/com/aman/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/aman/springboot/myfirstwebapp/todo/TodoService.java
@@ -8,15 +8,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
-private static List<Todo> todos = new ArrayList<>();
-static {
-	todos.add(new Todo(1, "in28Mins", "Learn AWS", LocalDate.now().plusYears(1), false));
-	todos.add(new Todo(2, "in28Mins", "Learn JAVA", LocalDate.now().plusYears(2), false));
-	todos.add(new Todo(3, "in28Mins", "Learn DSA", LocalDate.now().plusYears(3), false));
-	todos.add(new Todo(4, "in28Mins", "Learn JSP", LocalDate.now().plusYears(4), false));
-	todos.add(new Todo(5, "in28Mins", "Learn Spring", LocalDate.now().plusYears(5), false));
-}
-public List<Todo> findByUsername(String username) {
-	return todos;
-}
+	private static List<Todo> todos = new ArrayList<>();
+	private static int todosCount =0;
+	static {
+		todos.add(new Todo(++todosCount, "in28Mins", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "in28Mins", "Learn JAVA", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++todosCount, "in28Mins", "Learn DSA", LocalDate.now().plusYears(3), false));
+	}
+
+	public List<Todo> findByUsername(String username) {
+		return todos;
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);	
+	}
+	
+	
 }
